2188672b99d246e959b80b8a9da021de980335ee
gugmin <149454489+gugmin@users.noreply.github.com>
2023-11-30 14:09:17 +0900

    add endPanel
    시간, 엔드 판넬 추가함

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 게임매니저 싱글톤
+    public static GameManager I;
+
+    public GameObject endPanel;
+    public TMP_Text timeTxt;
+    public TMP_Text scoreTxt;
+    public TMP_Text maxScoreText;
+    public TMP_Text thisScoreText;
+
+    public float time;
+    float score;
+    public float maxScore;
+
+    private void Awake()
+    {
+        I = this; //싱글톤
+    }
+
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //남은 시간을 보여주는 기능
+        time -= Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        //제한시간 기능
+        if (time <= 0)
+        {
+            GameEnd();
+        }
+        //공이 바닥에 떨어져서 1개도 안남았을때
+
+    }
+
+    private void GameEnd()
+    {
+        Time.timeScale = 0f;
+        endPanel.SetActive(true);
+
+        if (score > maxScore)
+        {
+            maxScore = score;
+            maxScoreText.text = "최고점수 :" + score.ToString();
+        }
+        else if (score < maxScore)
+        {
+            thisScoreText.text = "현재점수 :" + score.ToString();
+        }
     }
 }
